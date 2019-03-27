98c6e3c4e7371e18a9da11485aa2975fc17bbbc5
Alex Dixon <alex.dixon10305@gmail.com>
2019-03-27 20:38:35 +0000

    Removing unessesary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { heavyModularFrame, OutputType } from './types';
+import { OutputType } from './types';
 import { Calculator } from './components/Calculator';
 import { TypeSelector } from './components/TypeSelector';
 
